2faa8b9f6dc89accd487ce70396e3b68ef53b6db
ACayet <arthurcayet@hotmail.fr>
2020-04-02 09:41:38 +0200

    fonction analyser.py

--- src/Analyser.py
+++ src/Analyser.py
@@ -7,15 +7,16 @@ class Analyser :
     def __init__(self):
         super().__init__()
 
-    def analyseDF(self, data_frame):
-        df_analyse = data_frame
+    def prepareVarDF(self, df_analyse):
         df_analyse['data-ram'] = df_analyse['data-ram'].astype(int)
         df_analyse['data-stockage'] = df_analyse['data-stockage'].astype(int)
         df_analyse['data-batterie'] = df_analyse['data-batterie'].astype(int)
         df_analyse['data-prix'] = df_analyse['data-prix'].astype(float)
         df_analyse['data-note'] = df_analyse['data-note'].astype(float)
         df_analyse['data-nb_avis'] = df_analyse['data-nb_avis'].astype(int)
+        return df_analyse
 
+    def boiteMoustachePrixRam(self, df_analyse):
         #Boite a moustache du prix en fonction des Ram => par GB repartition du prix
         temp_df1 = df_analyse  [["data-prix","data-ram"]]
         ax = temp_df1.boxplot(column='data-prix',by='data-ram')
@@ -24,16 +25,19 @@ class Analyser :
         ax.set_xlabel('Nb de Gb de RAM')
         plt.show()
 
+    def camembertStockage(self, df_analyse):
         #Repartition du stockage
         temp_df1 = df_analyse  [["data-stockage"]]
         temp_df1['data-stockage'].value_counts().plot.pie().set_title("Repartition du stockage")
         plt.show()
 
+    def camembertRam(self, df_analyse):
         #Repartition Ram
         temp_df1 = df_analyse  [["data-ram"]]
         temp_df1['data-ram'].value_counts().plot.pie().set_title("Repartition de la RAM")
         plt.show()
 
+    def densitePrix(self, df_analyse):
         #Fonction de densité du prix
         temp_df1 = df_analyse  [["data-prix"]]
         sns.distplot(temp_df1['data-prix'], hist=True, kde=True, 
@@ -42,7 +46,7 @@ class Analyser :
              kde_kws={'linewidth': 4}).set_title("Fonction de densité et histogramme par prix")
         plt.show()
 
-
+    def densiteBatterie(self, df_analyse):
         #fonction de densité du niveau de batterie
         temp_df1 = df_analyse  [["data-batterie"]]
         sns.distplot(temp_df1['data-batterie'], hist=True, kde=True, 
@@ -51,8 +55,7 @@ class Analyser :
              kde_kws={'linewidth': 4}).set_title("Fonction de densité et histogramme par niveau de batterie")
         plt.show()
 
-        
-
+    def evolvePrixMoyenRam(self, df_analyse):
         #Courbe d'évolution du prix moyen en fonction des gb de ram 
         temp_df1 = df_analyse  [["data-prix","data-ram"]]
         temp_df1 = temp_df1.groupby(temp_df1["data-ram"])["data-prix"].mean()
@@ -61,3 +64,27 @@ class Analyser :
         ax1.set_xlabel('Nb de Gb de RAM')
         ax1.set_ylabel('prix moyen en €')
         plt.show()
+
+    def analyseDF(self, data_frame):
+        df_analyse = data_frame
+        df_analyse = self.prepareVarDF(df_analyse)
+
+        self.boiteMoustachePrixRam(df_analyse)
+        self.camembertStockage(df_analyse)
+        self.camembertRam(df_analyse)
+        self.densitePrix(df_analyse)
+        self.densiteBatterie(df_analyse)
+        self.evolvePrixMoyenRam(df_analyse)
+
+        
+
+        
+
+        
+
+
+        
+
+        
+
+        
